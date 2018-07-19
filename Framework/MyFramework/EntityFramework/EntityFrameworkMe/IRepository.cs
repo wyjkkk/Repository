dbc4a8b9b690bddb75cc5956e5165399e592a04d
wang.yingjie <wang.yingjie@DST89069>
2018-07-19 17:13:10 +0800

    创建Repository

---
+++ Framework/MyFramework/EntityFramework/EntityFrameworkMe/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.EntityFrameworkMe
+{
+    interface IRepository<Entity>
+        where Entity:class
+    {
+        void Add(Entity entity);
+
+        void Delete(Entity entity);
+
+        void Update(Entity entity);
+
+        void Save();
+    }
+}
---
+++ Framework/MyFramework/EntityFramework/EntityFrameworkMe/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.EntityFrameworkMe
+{
+    public class RepositoryBase<Entity> : IRepository<Entity>
+         where Entity : class
+    {
+        public RepositoryBase(DbContext dbcontex)
+        {
+            this.dbcontex = dbcontex;
+        }
+
+        public void Add(Entity entity)
+        {
+            dbcontex.Set<Entity>().Add(entity);
+        }
+
+        public void Delete(Entity entity)
+        {
+            dbcontex.Set<Entity>().Remove(entity);
+        }
+
+        public void Update(Entity entity)
+        {
+            //dbcontex.Set<Entity>().en
+        }
+
+        public void Save()
+        {
+            dbcontex.SaveChanges();
+        }
+
+        private DbContext dbcontex;
+    }
+}
