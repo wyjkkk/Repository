efc833fe99afcb1e2a99e7a10d21ad51101f7d3f
wang.yingjie <wang.yingjie@DST89069>
2018-07-19 15:23:59 +0800

    Niject创建

---
+++ Framework/Niject/Niject/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niject
+{
+    public class NiJectManagement : INijectManagement
+    {
+        public void Initialize(string[] str)
+        {
+            
+        }
+    }
+}
---
+++ Framework/Niject/Niject/INJiectManagement.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Niject
+{
+    public interface INijectManagement
+    {
+        void Initialize(string[] str);
+    }
+}
